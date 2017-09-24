8d67e7214ac0cf2986648ae84a2f7a211556a40a
Karl Pokus <karl.pokus@gmail.com>
2017-09-24 16:24:42 +0200

    add rejectionHandler to axios

--- curl.js
+++ curl.js
@@ -4,11 +4,12 @@
 
 const args = require('minimist')(process.argv.slice(2));
 const axios = require('axios');
-const logger = (res) => console.log(res.data);
 
 const method = args._[0];
 const url = 'http://localhost:8000/api/v1/users' + args._[1];
 const name = args._[2];
 const data = name ? { name } : null;
 
-axios({ method, url, data }).then(logger);
+axios({ method, url, data })
+  .then(res => console.log(res.data))
+  .catch(err => console.error('unable to complete request'));
